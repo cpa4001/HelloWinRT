b81668af4445e61b34fc4e65e63495acb8d31d04
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2021-09-12 18:23:03 -0400

    Add project files.

---
+++ pch.h
@@ -0,0 +1,6 @@
+// pch.h
+#pragma once
+//Window APIs
+#include <winrt/Windows.Foundation.Collections.h>	//From Windows SDK
+#include <winrt/Windows.Web.Syndication.h>			//From Windoes SDK
+#include <iostream>
